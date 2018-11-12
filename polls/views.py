224bf14be228dceb1874036d15649fbb0f565809
ShikhaGangwar <shikhag6@gmail.com>
2018-11-12 22:33:42 +0000

    Public interface creation

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,21 @@
+from django.http import HttpResponse, Http404
+from django.template import loader
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Question
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello. You are at polls view")
+    latest_question_list =Question.objects.order_by('-pub_date')[:3]
+    template = loader.get_template('polls/index.html')
+    context = {'latest_question_list': latest_question_list}
+    return HttpResponse(template.render(context, request))
+
+def details(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/index.html', {'questions': question})
+
+def results(request, question_id):
+    return HttpResponse("You are looking at the results of %s "%question_id)
